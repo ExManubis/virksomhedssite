59d2f5e4fbc8307cd5682006b32dd5d1d06e1aa8
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-12-12 08:46:14 +0100

    fix(menu): fix small menu bugs
    See title.

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,8 @@ const menuLinks = document.querySelector(".menu_links");
 // VARIABLES
 let toggle = false;
 
-let y = 100;
-let c = 100;
+let y = 100; // y-axis
+let c = 100; // curve
 
 // FUNCTIONS
 function linearInterpol(start, end, time) {
@@ -16,15 +16,15 @@ function linearInterpol(start, end, time) {
 
 function animate() {
   if (toggle) {
-    y = linearInterpol(y, 0, 0.045).toFixed(2);
+    y = linearInterpol(y, 0, 0.035).toFixed(2);
     c = linearInterpol(c, 0, 0.065).toFixed(2);
     path.setAttribute(
       "d",
       `M 0 ${y} L 0 100 100 100 100 ${y} C ${50} ${c}, ${50} ${c}, 0 ${y}`,
     );
   } else {
-    y = linearInterpol(y, 100, 0.065).toFixed(2);
-    c = linearInterpol(c, 100, 0.085).toFixed(2);
+    y = linearInterpol(y, 100, 0.035).toFixed(2);
+    c = linearInterpol(c, 100, 0.075).toFixed(2);
     path.setAttribute(
       "d",
       `M 0 ${y} L 0 100 100 100 100 ${y} C ${50} ${c}, ${50} ${c}, 0 ${y}`,
